4343602c6839676b13bed98715dca07d69cc5674
David Livet <david.livet@orange.fr>
2014-12-26 00:10:17 +0100

    Add query in ContentProvider Add unit test

--- app/src/main/java/wtf/sur/original/puissante/rapide/automobile/sopracovoit/data/CovoitProvider.java
+++ app/src/main/java/wtf/sur/original/puissante/rapide/automobile/sopracovoit/data/CovoitProvider.java
@@ -18,6 +18,7 @@ package wtf.sur.original.puissante.rapide.automobile.sopracovoit.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -26,23 +27,96 @@ import android.net.Uri;
  */
 public class CovoitProvider extends ContentProvider {
 
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private CovoitDbHelper mOpenHelper;
+
     private static final int WORKPLACE = 100;
     private static final int USER = 200;
     private static final int PATH = 300;
 
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = CovoitContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, CovoitContract.PATH_WORKPLACE, WORKPLACE);
+        matcher.addURI(authority, CovoitContract.PATH_PATH, PATH);
+        matcher.addURI(authority, CovoitContract.PATH_USER, USER);
+        return matcher;
+    }
+
     @Override
     public boolean onCreate() {
-        return false;
+        mOpenHelper = new CovoitDbHelper(getContext());
+        return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
+            // "workplace"
+            case WORKPLACE: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        CovoitContract.WorkplaceEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            // "user"
+            case USER: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        CovoitContract.UserEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            // "path"
+            case PATH: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        CovoitContract.PathEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+
+        // Use the Uri Matcher to determine what kind of URI this is.
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case WORKPLACE:
+                return CovoitContract.WorkplaceEntry.CONTENT_TYPE;
+            case PATH:
+                return CovoitContract.PathEntry.CONTENT_TYPE;
+            case USER:
+                return CovoitContract.UserEntry.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
     }
 
     @Override
