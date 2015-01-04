87901bfa3d69ef4e2019039c3246d27ae86a9a6b
David Livet <david.livet@orange.fr>
2015-01-04 18:06:47 +0100

    Add sync adapter

---
+++ app/src/main/java/wtf/sur/original/puissante/rapide/automobile/sopracovoit/sync/PathSyncAdapter.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Jérémie Boutoille, Jules Cantegril, Hugo Djemaa, Mickael Goubin, David Livet
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package wtf.sur.original.puissante.rapide.automobile.sopracovoit.sync;
+
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+import android.util.Log;
+
+public class PathSyncAdapter extends AbstractThreadedSyncAdapter {
+
+    private static final String TAG = PathSyncAdapter.class.getSimpleName();
+
+    public PathSyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+        Log.d(TAG, "onPerformSync");
+    }
+}
